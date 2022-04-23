e3696f8b2cca9d24ee5b633e1e7b93ad9604651c
Pierre-Marie Aubineau <pm@cataire.fr>
2022-04-23 18:57:29 +0200

    feat(codebehind): initial commit of the code-behind project

---
+++ src/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding4FunWorkshop
+{
+    class Program
+    {
+        ///<param name="region">Takes in the --region option from the code fence options in markdown</param>
+        ///<param name="session">Takes in the --session option from the code fence options in markdown</param>
+        ///<param name="package">Takes in the --package option from the code fence options in markdown</param>
+        ///<param name="project">Takes in the --project option from the code fence options in markdown</param>
+        ///<param name="args">Takes in any additional arguments passed in the code fence options in markdown</param>
+        ///<see>To learn more see <a href="https://aka.ms/learntdn">our documentation</a></see>
+        static int Main(
+            string region = null,
+            string session = null,
+            string package = null,
+            string project = null,
+            string[] args = null)
+        {
+            return region switch
+            {
+                "switch-statement" => new SwitchStatement().RockPaperScissors(),
+
+                null => RunAll(),
+                _ => MissingTag(region),
+            };
+        }
+
+        private static int MissingTag(string tag, bool region = true)
+        {
+            Console.WriteLine($"No code snippet configured for {(region ? "region" : "session")}: {tag}");
+            return 1;
+        }
+        private static int RunAll()
+        {
+            // 1- 5
+            new SwitchStatement().RockPaperScissors();
+
+            return 0;
+        }
+    }
+}
---
+++ src/SwitchStatement.cs
@@ -0,0 +1,85 @@
+﻿namespace Coding4FunWorkshop
+{
+    public class SwitchStatement
+    {
+        public enum Hand { Rock, Paper, Scissors };
+        public int RockPaperScissors()
+        {
+            var playerOneHands = new List<Hand>() { Hand.Scissors, Hand.Paper, Hand.Rock };
+            var playerTwoHands = new List<Hand>() { Hand.Rock, Hand.Paper, Hand.Scissors };
+
+            foreach (var playerOneHand in playerOneHands)
+            {
+                foreach (var playerTwoHand in playerTwoHands)
+                {
+                    if (GetWinnerExpression(playerOneHand, playerTwoHand) != GetWinner(playerOneHand, playerTwoHand))
+                    {
+                        Console.WriteLine($"Result not expected for match: {playerOneHand} vs {playerTwoHand}:" +
+                            $"Expected: {GetWinnerExpression(playerOneHand, playerTwoHand)}" +
+                            $"Result: {GetWinner(playerOneHand, playerTwoHand)}");
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        public string GetWinnerExpression(Hand playerOne, Hand playerTwo)
+        => (playerOne, playerTwo) switch
+        {
+            (Hand.Rock, Hand.Paper) => "Player 2 wins with Paper.",
+            (Hand.Rock, Hand.Scissors) => "Player 1 wins with Rock.",
+            (Hand.Paper, Hand.Rock) => "Player 1 wins with Paper.",
+            (Hand.Paper, Hand.Scissors) => "Player 2 wins with Scissors.",
+            (Hand.Scissors, Hand.Rock) => "Player 2 wins with Rock.",
+            (Hand.Scissors, Hand.Paper) => "Player 1 wins with Scissors.",
+            (_, _) => "It's a tie."
+        };
+
+        #region switch-statement
+        public string GetWinner(Hand playerOne, Hand playerTwo)
+        {
+            switch(playerOne)
+            { 
+                case Hand.Rock:
+                    switch(playerTwo)
+                    {
+                        case Hand.Paper:
+                            return "Player 2 wins with Paper.";
+                        case Hand.Scissors:
+                            return "Player 1 wins with Rock.";
+                        default: 
+                            return "It's a tie";
+                    }
+                case Hand.Paper:
+                    switch (playerTwo)
+                    {
+
+                        case Hand.Rock:
+                            return "Player 1 wins with Paper.";
+                        case Hand.Scissors:
+                            return "Player 2 wins with Scissors.";
+                        default:
+                            return "It's a tie";
+                    }
+                    break;
+                case Hand.Scissors:
+                    switch (playerTwo)
+                    {
+
+                        case Hand.Rock:
+                            return "Player 2 wins with Rock.";
+                        case Hand.Paper:
+                            return "Player 1 wins with Scissors.";
+                        default:
+                            return "It's a tie";
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        #endregion
+
+    }
+}
