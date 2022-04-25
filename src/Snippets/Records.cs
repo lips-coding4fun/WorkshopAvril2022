75933f4967eae4ac6133358a066bd39d0a4162a7
Pierre-Marie Aubineau <pm@cataire.fr>
2022-04-25 21:02:28 +0200

    feat(workshop): added records snippet

---
+++ src/Snippets/Records.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding4FunWorkshop.Snippets
+{
+    public class Records
+    {
+    }
+
+    #region try-records
+    // Sans modifier CheckRecords, comment faire pour que 
+    public class CheckRecords
+    {
+        public static bool IsSamePage(CapturedPage pageA, CapturedPage pageB)
+        {
+            return pageA.Address == pageB.Address;
+        }
+    }
+
+    public class CapturedPage
+    {
+        public PageAdress Address;
+        public DateTime ScrapTime;
+        public byte[] Content;
+    }
+    public class PageAdress
+    {
+        public string Domain;
+        public string PageUri; 
+    }
+    #endregion
+    
+}
